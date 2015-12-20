e4795ceaea14ffb3c8736690b914f7790fd41a7e
MikeC1995 <mike_christensen@btinternet.com>
2015-12-20 18:59:23 +0000

    basic projects section

---
+++ app/components/projects/projectsController.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var portfolioApp = angular.module('portfolioApp');
+
+portfolioApp.controller('projectsController', ['$scope', function ($scope) {
+
+  var rootPath = "/assets/images/projects/";
+  // Set nav items
+  $scope.projectsItems = {
+    'Ocean Blue Software': rootPath + 'oceanblue/thumb.png',
+    'Environment Agency': rootPath + 'ea/thumb.jpg',
+    'Language Pear': rootPath + 'languagepear/thumb.png',
+    'Mubaloo': rootPath + 'mubaloo/thumb.png',
+    'IntruderCam': rootPath + 'intrudercam/thumb.png',
+    'HairBeaut': rootPath + 'hairbeaut/thumb.png',
+    'Student Robotics': rootPath + 'sr/thumb.png'
+  };
+}]);
