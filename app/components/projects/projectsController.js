a1ff4a8a3ff76d514a92e33cc7227e40c9004824
MikeC1995 <mike_christensen@btinternet.com>
2015-12-21 19:22:59 +0000

    can open specific project data

--- app/components/projects/projectsController.js
+++ app/components/projects/projectsController.js
@@ -5,19 +5,67 @@ var portfolioApp = angular.module('portfolioApp');
 portfolioApp.controller('projectsController', ['$scope', function ($scope) {
 
   var rootPath = "/assets/images/projects/";
-  // Set nav items
-  $scope.projectsItems = {
-    'Ocean Blue Software': rootPath + 'oceanblue/thumb.png',
-    'Environment Agency': rootPath + 'ea/thumb.jpg',
-    'Language Pear': rootPath + 'languagepear/thumb.png',
-    'Mubaloo': rootPath + 'mubaloo/thumb.png',
-    'IntruderCam': rootPath + 'intrudercam/thumb.png',
-    'HairBeaut': rootPath + 'hairbeaut/thumb.png',
-    'Student Robotics': rootPath + 'sr/thumb.png'
+
+  $scope.projects = {
+    'Ocean Blue Software': {
+      thumb: rootPath + 'oceanblue/thumb.png',
+      graphic: rootPath + 'oceanblue/graphic.png',
+      description: "Software engineering internship in the digital television industry. Designed and implemented Ocean Blue’s current set top box user interface. Extended their proprietary backend technology suite DTVKit and integrated it with the UI.",
+      demoPath: '/documents/oceanblue.pdf',
+      background: '#2980b9'
+    },
+    'Environment Agency': {
+      thumb: rootPath + 'ea/thumb.jpg',
+      graphic: rootPath + 'ea/graphic.png',
+      description: "Lead UI/UX developer and client communication lead on a team hired to build a crossplatform mobile app visualising the EA’s various geographic data sets.",
+      demoPath: '/documents/ea.pdf',
+      background: '#1B5E20'
+    },
+    'Language Pear': {
+      thumb: rootPath + 'languagepear/thumb.png',
+      graphic: rootPath + 'languagepear/graphic.png',
+      description: "A cross-platform language practice platform.",
+      demoPath: '/documents/languagepear.pdf',
+      background: '#27ae60'
+    },
+    'Mubaloo': {
+      thumb: rootPath + 'mubaloo/thumb.png',
+      graphic: rootPath + 'mubaloo/graphic.png',
+      description: "Full-stack developer of Appiphany, a real-time location-based message sharing application.",
+      demoPath: '/documents/mubaloo.pdf',
+      background: '#3B3B38'
+    },
+    'IntruderCam': {
+      thumb: rootPath + 'intrudercam/thumb.png',
+      graphic: rootPath + 'intrudercam/graphic.png',
+      description: "Developed and released a motion detection application to the Windows Phone store, which now has 8000+ downloads and a rating of 4.8 stars.",
+      demoPath: '/documents/intrudercam.pdf',
+      background: '#F3EA29'
+    },
+    'HairBeaut': {
+      thumb: rootPath + 'hairbeaut/thumb.png',
+      graphic: rootPath + 'hairbeaut/graphic.png',
+      description: "Designed and built hairbeaut.com, a social network for the hair and beauty industry, using the Ning platform and CSS.",
+      demoPath: '/documents/hairbeaut.pdf',
+      background: '#CECFCE'
+    },
+    'Student Robotics': {
+      thumb: rootPath + 'sr/thumb.png',
+      graphic: rootPath + 'sr/graphic.png',
+      description: "Lead developer in team winning second place and the Committee Award in a national robotics competition.",
+      demoPath: '/documents/sr.pdf',
+      background: '#CECFCE'
+    }
   };
 
   $scope.currentProject = {
-    isOpen: true
+    isOpen: true,
+    name: 'Environment Agency'
+  }
+
+  $scope.openProject = function(projectName) {
+    $scope.currentProject.name = projectName;
+    $scope.currentProject.isOpen = true;
   }
 
 }]);
