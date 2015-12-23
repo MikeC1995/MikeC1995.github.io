f62dfe85ac0b1d092dd50d939da06daf11bcb07a
MikeC1995 <mike_christensen@btinternet.com>
2015-12-23 19:14:07 +0000

    responsive menu

--- app/partials/header/headerController.js
+++ app/partials/header/headerController.js
@@ -4,4 +4,17 @@ var cssApp = angular.module('portfolioApp');
 
 portfolioApp.controller('headerController', ['$scope', function ($scope) {
 
+  $scope.nav = {
+    state: false
+  }
+  $scope.toggleNav = function() {
+    $scope.nav.state = !$scope.nav.state;
+  }
+  /*$scope.nav.isOpen = function() {
+    if($scope.nav.open) {
+      return 'nav-open';
+    } else {
+      return 'nav-closed';
+    }
+  }*/
 }]);
